512c56aac20511ce431526eee34bd6972942798a
David Chen <davidc3287@gmail.com>
2022-03-22 10:39:16 -0700

    refactor

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from icecream import ic
 
 # pulled from set categories
 LICENSED = ['Avatar The Last Airbender', 'Batman', 'Brick Sketches', 'BrickHeadz', 'Cars', 'DC Comics Super Heroes', 'DC Super Hero Girls', 'Disney', 'Galidor', 'Ghostbusters', 'Harry Potter', 'Horizon', 'Indiana Jones', 'Jurassic World', 'Marvel Super Heroes', 'Minecraft', 'Minions: The Rise of Gru', 'Mixels', 'Overwatch', 'Pirates of the Caribbean', 'Prince of Persia', 'Scooby-Doo',
@@ -8,26 +9,46 @@ LICENSED = ['Avatar The Last Airbender', 'Batman', 'Brick Sketches', 'BrickHeadz
 
 API_KEY = '3-7NSU-8Pqh-eDwah'
 USER_HASH = 'krrngmEXdD'
-# method = 'getThemes'
-# URL = f'https://brickset.com/api/v3.asmx/{method}?apiKey={API_KEY}'
 
-# r = requests.get(URL)
-# with open('themes.json', 'w') as f:
-#     json.dump(r.json(), f)
-# themes = r.json()['themes']
 
-with open('themes.json') as f:
+
+
+def get_theme_names():
+    with open('themes.json') as f:
     themes = json.load(f)['themes']
 
-total_sets = 0
-c = 0
-for theme in themes:
-    total_sets += theme['setCount']
-    c += 1
-print(total_sets)
+    theme_names = []
+    total_sets = 0
+    c = 0
+    for theme in themes:
+        total_sets += theme['setCount']
+        theme_names.append(theme['theme'])
+        c += 1
+    
+    return theme_names
 
-method = 'getSets'
-params = json.dumps({'theme': 'Technic'})
-URL = f'https://brickset.com/api/v3.asmx/{method}?apiKey={API_KEY}&userHash={USER_HASH}&params={params}'
-r = requests.get(URL)
-print(r.json())
+
+def get_sets_by_theme(theme_name):
+
+    method = 'getSets'
+    params = json.dumps({'theme': 'theme_name'})
+    URL = f'https://brickset.com/api/v3.asmx/{method}?apiKey={API_KEY}&userHash={USER_HASH}&params={params}'
+    r = requests.get(URL)
+    print(r.json())
+    return r.json()['sets']
+
+
+def get_all_sets():
+    sets = []
+    for theme_name in theme_names:
+        ic(theme_name)
+        sets.append(get_sets_by_theme(theme_name))
+        break
+
+    with open('sets.json', 'w') as f:
+        f.write(json.dumps(sets))
+
+    return sets
+
+
+get_all_sets()
