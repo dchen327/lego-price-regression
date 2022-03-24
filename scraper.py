3df324a4b2924b63f8f12dd20dc9403ee20129f1
David Chen <davidc3287@gmail.com>
2022-03-24 00:40:04 -0700

    thresholds

--- scraper.py
+++ scraper.py
@@ -19,19 +19,24 @@ def get_theme_names():
     theme_names = []
     total_sets = 0
     for theme in themes:
-        total_sets += theme['setCount']
-        theme_names.append(theme['theme'])
+        if theme['setCount'] > 50:
+            total_sets += theme['setCount']
+            theme_names.append(theme['theme'])
 
+    ic(len(theme_names))
+    ic(total_sets)
     return theme_names
+
+
+get_theme_names()
 
 
 def get_sets_by_theme(theme_name):
 
     method = 'getSets'
-    params = json.dumps({'theme': theme_name})
+    params = json.dumps({'theme': theme_name, 'pageSize': 500})
     URL = f'https://brickset.com/api/v3.asmx/{method}?apiKey={API_KEY}&userHash={USER_HASH}&params={params}'
     r = requests.get(URL)
-    ic(len(r.json()['sets']))
     return r.json()['sets']
 
 
@@ -39,26 +44,34 @@ def load_sets():
     with open('sets.json') as f:
         return json.load(f)
 
+
+# count num of sets
+# c = 0
+# sets = load_sets()
+# for theme in sets:
+#     ic(len(theme))
+#     for set_ in theme:
+#         c += 1
+
 # first 80 done
 
 
-def get_all_sets():
-    sets = load_sets()
+# def get_all_sets():
+#     sets = load_sets()
 
-    theme_names = get_theme_names()
-    for theme_name in theme_names[80:100]:
-        ic(theme_name)
-        sets.append(get_sets_by_theme(theme_name))
+#     theme_names = get_theme_names()
+#     for theme_name in theme_names[100:]:
+#         ic(theme_name)
+#         sets.append(get_sets_by_theme(theme_name))
 
-    with open('sets.json', 'w') as f:
-        f.write(json.dumps(sets))
+#     with open('sets.json', 'w') as f:
+#         f.write(json.dumps(sets))
 
-    return sets
+#     return sets
 
-
-# get_all_sets()
 
 # print(get_theme_names()[60:80])
+
 
 def create_df():
     sets = []
@@ -71,7 +84,7 @@ def create_df():
     df = pd.DataFrame(sets)
     df['licensed'] = df['name'].apply(lambda x: x in LICENSED)
 
-    print(df.head())
+    # print(df.head())
 
 
-create_df()
+# create_df()
